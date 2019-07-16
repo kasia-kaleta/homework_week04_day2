c47fc800be549a11eabc0d261a9a8573069817cb
kat3005 <kasia.alicja@gmail.com>
2019-07-16 18:43:11 +0100

    Ads ERB views file for result

--- start_point/controller.rb
+++ start_point/controller.rb
@@ -4,13 +4,21 @@ require('sinatra/reloader') if development?
 require_relative('./models/rps.rb')
 also_reload('./models/*')
 
-get '/rock/scissors' do
-   "rock wins"
-end
-
-# get '/check_win/:hand1/:hand2' do
-#   hand1 = params[:hand1]
-#   hand2 = params[:hand2]
-#   @game = RPSGame.check_win(hand1, hand2)
-#   erb(:result)
+# get '/rock/scissors' do
+#    "rock wins"
 # end
+#
+# get '/scissors/paper' do
+#    "scissors wins"
+# end
+#
+# get '/paper/rock' do
+#    "paper wins"
+# end
+
+get '/check_win/:hand1/:hand2' do
+  hand1 = params[:hand1]
+  hand2 = params[:hand2]
+  @game = RPSGame.check_win(hand1, hand2)
+  erb(:result)
+end
