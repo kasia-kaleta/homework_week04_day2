ded8fd48f76466c375640c330654bbedb7c6d3dc
kat3005 <kasia.alicja@gmail.com>
2019-07-16 15:34:48 +0100

    Initial commit

---
+++ start_point/controller.rb
@@ -0,0 +1,12 @@
+require('sinatra')
+require('sinatra/reloader') if development?
+
+require_relative('./models/rps.rb')
+also_reload('./models/*')
+
+get '/check_win/:hand1/:hand2' do
+  hand1 = params[:hand1]
+  hand2 = params[:hand2]
+  @game = RPSGame.check_win(hand1, hand2)
+  erb(:result)
+end
