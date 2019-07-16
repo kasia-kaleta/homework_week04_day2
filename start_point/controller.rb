37a9f4b823f64d92c991363b5afb9705065bafc4
kat3005 <kasia.alicja@gmail.com>
2019-07-16 16:01:33 +0100

    Adds models directory

--- start_point/controller.rb
+++ start_point/controller.rb
@@ -4,9 +4,13 @@ require('sinatra/reloader') if development?
 require_relative('./models/rps.rb')
 also_reload('./models/*')
 
-get '/check_win/:hand1/:hand2' do
-  hand1 = params[:hand1]
-  hand2 = params[:hand2]
-  @game = RPSGame.check_win(hand1, hand2)
-  erb(:result)
+get '/rock/scissors' do
+   "rock wins"
 end
+
+# get '/check_win/:hand1/:hand2' do
+#   hand1 = params[:hand1]
+#   hand2 = params[:hand2]
+#   @game = RPSGame.check_win(hand1, hand2)
+#   erb(:result)
+# end
